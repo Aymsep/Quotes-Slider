027677ea3a4f020dbc6716824bb540102b126fe6
Aymsep <71574687+Aymsep@users.noreply.github.com>
2022-11-17 13:58:24 +0100

    slider quotes done

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 
 window.addEventListener("DOMContentLoaded",function(){
+    // Local data 
     const review = [
         {
             img :'./images/simon.png',
             name : 'simon sinek',
             job : 'Simon Oliver Sinek is an American author and inspirational speaker. He is the author of five books, including Start With Why and The Infinite Game.',
-            info : 'Great companies dont hire skilled people and motivate them, they hire already motivated people and inspire them.',
+            quote : 'Great companies dont hire skilled people and motivate them, they hire already motivated people and inspire them.',
         },
     
         {
             img :'./images/martin.png',
             name : 'Martin Luther King Jr.',
             job : 'Martin Luther King Jr. was an American Baptist minister and activist, one of the most prominent leaders in the civil rights movement from 1955 until his assassination in 1968.',
-            info:'I believe that unarmed truth and unconditional love will have the final word in reality.',
+            quote:'I believe that unarmed truth and unconditional love will have the final word in reality.',
     
     
         },
@@ -21,7 +22,7 @@ window.addEventListener("DOMContentLoaded",function(){
             img :'./images/james.png',
             name : 'James Clear',
             job:'James Clear is an author, entrepreneur, and photographer in 25+ countries. JamesClear.com is the home of his writing and work.',
-            info : 'You should be far more concerned with your current trajectory than with your current results.',
+            quote : 'You should be far more concerned with your current trajectory than with your current results.',
     
         },
     
@@ -29,26 +30,35 @@ window.addEventListener("DOMContentLoaded",function(){
             img :'./images/hitler.png',
             name : 'Adolf Hitler',
             job : 'Adolf Hitler was an Austrian-born German politician who was dictator of Germany from 1933 until his death in 1945',
-            info:'And I can fight only for something that I love, love only what I respect, and respect only what I at least know.',
+            quote:'And I can fight only for something that I love, love only what I respect, and respect only what I at least know.',
         }
     ]
-    const image = document.getElementById("author-img")
-    const name = document.getElementById("author-name")
-    const job = document.getElementById("author-job")
-    const info = document.getElementById("author-info")
+
+    // declaring variables
+    const image = document.getElementById("author-img")//get author image
+    const name = document.getElementById("author-name")//get author name
+    const job = document.getElementById("author-job")//get author job
+    const quote = document.getElementById("author-quote")//get author quote
     
-    const right_arrow = document.querySelector(".right")
-    const left_arrow = document.querySelector(".left")
-    let current = 0;
+    const right_arrow = document.querySelector(".right")//right arrow
+    const left_arrow = document.querySelector(".left")//left arrow
+
+    let current = 0;//current
+
+    //function to display author info in html
     function display_author(author) {
         const item = review[author]
         image.src = item.img
         name.innerHTML = item.name
         job.innerHTML = item.job
-        info.innerHTML = item.info
+        quote.innerHTML = item.quote
     }
     
+
+    //on click right arrow , execute right_click
     right_arrow.addEventListener('click', right_click)
+
+    //right click function
     function right_click(e){
         e.preventDefault()
         current++;
@@ -58,7 +68,10 @@ window.addEventListener("DOMContentLoaded",function(){
         display_author(current)
     }
     
+    //on click left arrow , execute left_click
     left_arrow.addEventListener('click', left_click)
+    
+    //left_click function
     function left_click(e){
         console.log('enter current '+current)
        e.preventDefault()
